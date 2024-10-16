68427309cc5313948c501f999330814f7e6d1b25
nate mccoy <nathan.mccoy@gmail.com>
2024-10-16 11:22:14 -0400

    Add YAML configuration parsing with serde
    Introduced serde and serde_yaml dependencies to handle YAML parsing. Modified main function to read and parse config file, and added structs for the configuration. Added a sample config.yaml file for demonstration purposes.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+mod structs;
+
+use std::{env, fs};
+use std::error::Error;
+use structs::Config;
+
+fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        return Err("Usage: {} <config_file.yaml>".into());
+    }
+
+    let config_file = &args[1];
+    let config = read_config(config_file)?;
+
+
+    println!("obsidian_knife made the cut with {}", config_file);
+    Ok(())
+}
+
+pub fn read_config(config_file: &str) -> Result<Config, Box<dyn Error + Send + Sync>> {
+    let contents = fs::read_to_string(config_file)?;
+    let config: Config = serde_yaml::from_str(&contents)?;
+    Ok(config)
 }
