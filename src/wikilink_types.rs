71d225725d70932915efeb983285b0cd1050067c
nate mccoy <nathan.mccoy@gmail.com>
2024-11-04 10:45:10 -0500

    Add Malformed case to InvalidWikilinkReason enum
    Included a new `Malformed` variant in the `InvalidWikilinkReason` enum to handle catchall invalid cases for wikilinks. Corresponding match arm was updated to provide an appropriate error message.

--- src/wikilink_types.rs
+++ src/wikilink_types.rs
@@ -79,6 +79,7 @@ impl fmt::Display for Wikilink {
 pub enum InvalidWikilinkReason {
     DoubleAlias,       // e.g. [[A|B|C]]
     EmptyWikilink,     // [[]] or [[|]]
+    Malformed,         // catchall
     NestedOpening,     // Contains [[ inside
     UnmatchedClosing,  // ]] without matching [[
     UnmatchedOpening,  // [[ without closing ]]
@@ -92,6 +93,7 @@ impl fmt::Display for InvalidWikilinkReason {
         match self {
             Self::DoubleAlias => write!(f, "contains multiple alias separators"),
             Self::EmptyWikilink => write!(f, "contains empty wikilink"),
+            Self::Malformed => write!(f, "catchall"),
             Self::NestedOpening => write!(f, "contains nested opening brackets '[['"),
             Self::UnmatchedClosing => write!(f, "contains unmatched closing brackets ']]'"),
             Self::UnmatchedOpening => write!(f, "contains unmatched opening brackets '[['"),
